a6c960cd65deb528dd5b404bf60b51c23e9ab29d
Paola Outeda <paola.outeda@correo.unimet.edu.ve>
2019-11-03 04:52:23 -0400

    agregando validacion en caso de funcion agotada

--- src/app/components/movie-details/movie-details.component.ts
+++ src/app/components/movie-details/movie-details.component.ts
@@ -3,6 +3,7 @@ import { PeliculaService } from "src/app/services/pelicula.service";
 import { ActivatedRoute } from "@angular/router";
 import { Router } from "@angular/router";
 import { Pelicula } from "../../models/pelicula";
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Component({
   selector: "app-movie-details",
@@ -38,12 +39,26 @@ export class MovieDetailsComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private peliculaService: PeliculaService
+    private peliculaService: PeliculaService,
+    private db: AngularFirestore
   ) {}
 
   ngOnInit() {
     let id = this.route.snapshot.paramMap.get("id");
     this.movieId = id;
+
+    this.db
+      .collection("ordenes", ref => ref.where("pelicula", "==", this.movieId))
+      .get()
+      .subscribe(res => {
+        this.reservas = res.docs.reduce((acc: number, curr: any) => {
+          acc += curr.data().cantidad;
+          return acc;
+        }, 0);
+        if (this.reservas >= 30) {
+          this.disponible = false;
+        }
+      });
     this.peliculaService.getMovies().subscribe((data: Pelicula[]) => {
       this.movies = data;
       this.filtrarPeliculas();
