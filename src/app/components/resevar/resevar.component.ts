3ed0aff44ec9829d08849736cc2f023d87065b76
Paola Outeda <paola.outeda@correo.unimet.edu.ve>
2019-11-03 03:00:02 -0400

    Agregando validación

--- src/app/components/resevar/resevar.component.ts
+++ src/app/components/resevar/resevar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: "app-resevar",
@@ -10,7 +12,12 @@ export class ResevarComponent implements OnInit {
   reservarForm: FormGroup;
   submitted = false;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private db: AngularFirestore
+  ) {}
 
   ngOnInit() {
     this.reservarForm = this.formBuilder.group({
@@ -25,14 +32,32 @@ export class ResevarComponent implements OnInit {
 
   onSubmit() {
     this.submitted = true;
-
-    // stop here if form is invalid
-    if (this.reservarForm.invalid) {
-      return;
-    }
-    // display form values on success
-    alert(
-      "SUCCESS!! :-)\n\n" + JSON.stringify(this.reservarForm.value, null, 4)
+    let ordenesRef = this.db.collection("ordenes", ref =>
+      ref.where("pelicula", "==", this.route.snapshot.paramMap.get("id"))
     );
+
+    ordenesRef.get().subscribe(res => {
+      const reservados = res.docs.reduce((acc: number, curr: any) => {
+        acc += curr.data().cantidad;
+        return acc;
+      }, 0);
+      if (
+        reservados < 30 &&
+        reservados + Number(this.reservarForm.value.entradas) <= 30
+      ) {
+        this.db
+          .collection("ordenes")
+          .add({
+            cantidad: Number(this.reservarForm.value.entradas),
+            email: this.reservarForm.value.email,
+            fecha: new Date(),
+            pelicula: this.route.snapshot.paramMap.get("id")
+          })
+          .then(res => {
+            alert("Reserva exitosa");
+            this.router.navigateByUrl("/home");
+          });
+      } else alert(`Solo hay ${30 - reservados} disponibles`);
+    });
   }
 }
