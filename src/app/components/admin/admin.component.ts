155e7d87a23d228f7c2ef09c08fdd7281b4d57a7
Paola Outeda <paola.outeda@correo.unimet.edu.ve>
2019-11-03 03:53:47 -0400

    Agregando fecha y leyendo datos de firebase

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -3,26 +3,35 @@ import { AngularFirestore } from "@angular/fire/firestore";
 
 export interface Orden {
   id: number;
+  pelicula: string;
   correo: string;
   cantidad: number;
   fecha: string;
 }
 
-const ELEMENT_DATA: Orden[] = [
-  { id: 1, correo: "kilordpepo@gmail.com", cantidad: 2, fecha: "02-11-2019" },
-  { id: 2, correo: "carlospepo@msn.com", cantidad: 5, fecha: "01-11-2019" },
-  { id: 3, correo: "paolaouteda@gmail.com", cantidad: 8, fecha: "12-10-2019" },
-  { id: 4, correo: "piso213@gmail.com", cantidad: 1, fecha: "02-11-2019" }
-];
+const ELEMENT_DATA: Orden[] = [];
 @Component({
   selector: "app-admin",
   templateUrl: "./admin.component.html",
   styleUrls: ["./admin.component.css"]
 })
 export class AdminComponent implements OnInit {
-  displayedColumns: string[] = ["id", "correo", "cantidad", "fecha"];
+  displayedColumns: string[] = ["id", "pelicula", "email", "cantidad", "fecha"];
   dataSource = ELEMENT_DATA;
-  constructor(db: AngularFirestore) {}
+  constructor(private db: AngularFirestore) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.db
+      .collection("ordenes")
+      .get()
+      .subscribe(data => {
+        const ordenes: Orden[] = [];
+        data.docs.map(doc => {
+          const obj: any = { ...doc.data(), id: doc.id };
+          ordenes.push(obj);
+          console.log(obj);
+        });
+        this.dataSource = ordenes;
+      });
+  }
 }
